65d1490cd53a1c14e32c5456a956a80f8f8afe2e
Jimenez-hazibe <109534485+Jimenez-hazibe@users.noreply.github.com>
2022-08-01 17:30:38 -0500

    Add files via upload

---
+++ curso.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 01-08-2022 a las 22:30:18
+-- Versión del servidor: 10.4.10-MariaDB
+-- Versión de PHP: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `curso`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnos`
+--
+
+DROP TABLE IF EXISTS `alumnos`;
+CREATE TABLE IF NOT EXISTS `alumnos` (
+  `id` int(50) NOT NULL AUTO_INCREMENT,
+  `nombre` int(200) NOT NULL,
+  `primerApellido` int(100) NOT NULL,
+  `segundoApellido` int(100) NOT NULL,
+  `curp` int(20) NOT NULL,
+  `programa_educativo` int(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
